07b9a7678c3cc1e0e2a539ef5d26d15ad88d1d5f
Rizal Mutaqin <zanxzhui@gmail.com>
2020-01-29 13:05:12 +0700

    Add Dashboard Page

--- lib/view/dashboard.dart
+++ lib/view/dashboard.dart
@@ -9,15 +9,21 @@ class DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Dashboard"),
-        centerTitle: true,
-      ),
-      body: new Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[Text("Dashboard")],
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Dashboard"),
+          centerTitle: true,
+        ),
+        body: new Center(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 9.0, vertical: 9.0),
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text("Dashboard"),
+              ],
+            ),
+          ),
+        ));
   }
 }
